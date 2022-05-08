5c947323519a7892ce743be4b01d054d3feb6c25
Kranold <rolfkranold@gmail.com>
2022-05-08 20:40:07 +0200

    fix cors

--- server.js
+++ server.js
@@ -3,7 +3,13 @@ const app = express()
 require('dotenv').config()
 
 const cors = require('cors')
-app.use(cors())
+
+const corsOptions ={
+    origin:'*', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200,
+ }
+app.use(cors(corsOptions))
 
 app.use(express.json())
 
